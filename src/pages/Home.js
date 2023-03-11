46b2264da87b065f9da8ed8df0690e7b60a79d4c
BotJB <69150052+BotJB@users.noreply.github.com>
2023-03-11 11:15:29 -0500

    Implemented pokemon API

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,36 @@
-import React from 'react'
+import React,{useEffect,useState} from 'react'
 
 const Home = () => {
+const [pokemon,setPokemon]=useState({
+  name:'',
+  img:'',
+  abilites:[],
+  height:'',
+  weight:''
+})
+
+
+useEffect(()=>{
+const getData=async()=>{
+  const response=await fetch('https://pokeapi.co/api/v2/pokemon/pikachu')
+  const json=await response.json()
+  console.log(json)
+  setPokemon({
+    name:json.name,
+    img:json.sprites.other.dream_world.front_default,
+    abilites:json.abilities,
+    height:json.height,
+    weight:json.weight
+  })
+}
+getData()
+},[])
+console.log(pokemon)
   return (
-    <div>Home</div>
+    <div>
+
+     
+    </div>
   )
 }
 
